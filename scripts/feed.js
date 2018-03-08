c011ef1a47e47f272416a9b6ec092dcad0713394
root <root@ubuntu-1gb-nyc3-01.localdomain>
2018-03-08 13:41:42 +0000

    More features to feed page

--- scripts/feed.js
+++ scripts/feed.js
@@ -3,12 +3,12 @@ let posts = []
 window.onload = () => {
   fetch('http://jacobsimonson.me:3000/feed', {method: 'GET'})
   .then( res => {return res.json()})
-  .then( res => {for(post of res) posts.push(post.content)})
+  .then( res => {for(post of res) posts.push(post)})
 }
 
 const app = new Vue({
   el: '#feed-list',
   data: {
-    content: posts
+    posts
   }
 })
