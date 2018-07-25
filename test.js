f91e041b7adc9225f41418a0d3e81850c208c332
Jacob Simonson <jsimonson2013@gmail.com>
2018-07-25 05:44:40 -0700

    Using promise all. Added test for bypass. Need to do teardown.

--- test.js
+++ test.js
@@ -5,20 +5,31 @@ console.log('=============================TESTING STARTED=======================
 testLogin = new Promise((resolve, reject) => {
 	fetch('http://localhost:3000/login/?user=test&pass=pass', {method: 'GET'})
 	.then(res => {
-		if (res.status === 200) {
-			resolve(true)	
-		}
-		else {
-			resolve(false)
-		}
+		if (res.status === 200) resolve(true)
+		else resolve(false)
+
+		reject()
+	})
+})
+
+testByPass = new Promise((resolve, reject) => {
+	fetch('http://localhost:3000/bypass/?user=3', {method: 'GET'})
+	.then(res => {
+		if (res.status === 200) resolve(true)
+		else resolve(false)
+
 		reject()
 	})
 })
 
 runtests = () => {
+	const tests = []
 
-	testLogin.then(passed => {
-		console.log(passed)
+	tests.push(testByPass)
+	tests.push(testLogin)
+
+	Promise.all(tests).then((res) => {
+		console.log(res)
 		console.log('\n\n=============================TESTING ENDED============================')
 
 		process.exit()
