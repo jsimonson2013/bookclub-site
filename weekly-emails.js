db73b1a586c81c9bef0098dca705ac3c743c1b54
jsimonson2013 <jsimonson2013@gmail.com>
2018-08-07 02:15:24 +0000

    Need to figure out how to exit after sending emails.

---
+++ weekly-emails.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const mysql = require('mysql')
+const sendmail = require('sendmail')()
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: process.argv[2],
+	password: process.argv[3],
+	database: process.argv[4]
+})
+
+const sendEmail = (recipient, subject, body) => {
+	if (recipient != 'jsimonson2013@gmail.com') return
+
+	sendmail({
+		from: 'webmaster@jacobsimonson.me',
+		to: recipient,
+		subject: subject,
+		html: `Hello,<br><br>${body}<br><br>Have a nice day!<br><br><a href="https://friendgroup.jacobsimonson.me">Link to FriendGroup.</a>`,
+	}, (err, reply) => {if (err) console.log(err)})
+}
+
+const getTopPosts = () => {
+	const queries = []
+
+	connection.query(`select group_id, name from groups;`, (err, rows, fields) => {
+		if (err) throw err
+
+		for (let row of rows) {
+			queries.push(new Promise((resolve, reject) => {
+				connection.query(`select content, author from posts where group_id = ${row.group_id} and parent_id is NULL order by DATE(date) desc limit 3;`, (err, r, fields) => {
+					if (err) throw err
+
+					const posts = []
+					for (let post of r) posts.push({'content': post.content, 'author': post.author, 'gid': row.group_id, 'gname': row.name})
+
+					resolve(posts)
+				})
+			}))
+		}
+		groupPostsByUsers(queries)
+	})
+}
+
+const groupPostsByUsers = promises => {
+	const queries = []
+
+	Promise.all(promises).then(groups => {
+		for (let post of groups) {
+			if (!post[0]) continue
+
+			queries.push(new Promise((resolve, reject) => {
+				connection.query(`select email from users where default_group_id=${post[0].gid} and pass is not NULL;`, (err, rows, fields) => {
+					if (err) throw err
+
+					const emails = []
+					for (let email of rows) emails.push({'email': email.email, 'posts': post})
+
+					resolve(emails)
+				})
+			}))
+		}
+		sendEmails(queries)
+	})
+}
+
+// TODO: Exit after...
+const sendEmails = promises => {
+	Promise.all(promises).then(groups => {
+		for (let emails of groups) {
+			for (let email of emails) {
+				let body = '<p>Here is a list of the top posts in your default group:</p><ul>'
+				for (let post of email.posts) {
+					body = body + '<li>'+post.author+' wrote "'+post.content+'" in '+post.gname+'!</li>'
+				}
+				sendEmail(email.email, 'FriendGroup Top Posts this Week!', body + '</ul><p>To check these out follow the link at the bottom of this email.</p>')
+			}
+		}
+	})
+}
+
+getTopPosts()
